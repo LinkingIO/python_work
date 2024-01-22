9dbc280f1a2e5a12751c0ac3c64ddcfa251b94d5
luo <rockyluo@163.com>
2024-01-22 21:53:05 +0800

    add unitest

--- chapter11_language_survey.py
+++ chapter11_language_survey.py
@@ -20,32 +20,44 @@ class AnonymousSurvey():
         print("Survey results:")
         print(' - '.join(self.responses))
 
-#定义一个问题
-question = "What language did you first learn to speak?"
-my_survey = AnonymousSurvey(question)
+# #定义一个问题
+# question = "What language did you first learn to speak?"
+# my_survey = AnonymousSurvey(question)
         
-# 显示问题并存储答案
-my_survey.show_question()
-print("Enter 'q' at any time to quit.\n")
-while(True):
-    response = input('Language: ')
-    if response == 'q':
-        break
-    my_survey.store_response(response)
+# # 显示问题并存储答案
+# my_survey.show_question()
+# print("Enter 'q' at any time to quit.\n")
+# while(True):
+#     response = input('Language: ')
+#     if response == 'q':
+#         break
+#     my_survey.store_response(response)
         
-# 显示调查结果
-print("\nThank you to everyone who participated in the survey!")
-my_survey.show_results()
+# # 显示调查结果
+# print("\nThank you to everyone who participated in the survey!")
+# my_survey.show_results()
         
 class TestAnonymousSurvey(unittest.TestCase):
     """针对AnonymousSurvey类的测试"""
 
-def test_store_single_response(self):
-    """测试单个答案是否符合"""
-    question = "What language did you first learn to speak?"
-    my_survey = AnonymousSurvey(question)
-    my_survey.store_response('Mandarin')
+    def test_store_single_response(self):
+        """测试单个答案是否符合"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('Mandarin')
 
-    self.assertIn('English', my_survey.responses)
+        self.assertIn('Mandarin', my_survey.responses)
+
+    def test_store_three_responses(self):
+        """测试 3 个答案是否正确"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['Mandarin', 'English', 'Japanise']
+
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 unittest.main()
